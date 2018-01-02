9be54237e90b6e04cf0e8599e459ea51cbb24086
psimoes <pms@uninova.pt>
2018-01-02 17:45:12 +0000

    wrote hello world

---
+++ hello-world/HelloWorld.pm
@@ -0,0 +1,11 @@
+# Declare package 'HelloWorld' with version
+package HelloWorld 1;
+use strict;
+use warnings;
+
+sub hello {
+  # Remove the comments and write some code here to pass the test suite.
+  return 'Hello, World!'
+}
+
+1;
