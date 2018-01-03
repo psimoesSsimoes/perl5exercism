4142863dfbd8ba8277ad7cbe56d7c279b49278b6
psimoes <pms@uninova.pt>
2018-01-03 09:01:57 +0000

    solved bob looking at implementation. perl is a strange wondurful language

--- bob/Bob.pm
+++ bob/Bob.pm
@@ -2,22 +2,16 @@
 package Bob 2;
 use strict;
 use warnings;
-use Switch;
 
 sub hey {
-  my ($msg) = @_;
- 
-  return $msg;
-
-  if (length $msg == 0){
-	return {"Fine. Be that way!"}
-  }
-  switch(substr $msg, -1){
-	  case "!" {return "Whoa, chill out!"}
-	  case "?" {return "Sure."}
-	  else {return "Whatever."}
-  }
-  
+my ($text) = @_;
+  return 'Fine. Be that way!'
+    if $text =~ /^\s*$/;
+  return 'Whoa, chill out!'
+    if uc($text) eq $text and $text =~ /\p{Uppercase}/;
+  return 'Sure.'
+    if $text =~ /\?\s*$/;
+  return 'Whatever.'; 
 }
 
 1;
