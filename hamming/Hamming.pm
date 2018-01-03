8ee7d57bd1ceb9645c33d5a24d8b408bb53ea17b
psimoes <pms@uninova.pt>
2018-01-03 22:43:39 +0000

    hamming completed

--- hamming/Hamming.pm
+++ hamming/Hamming.pm
@@ -3,6 +3,21 @@ use strict;
 use warnings;
 
 sub compute {
-	my $a 	
+	my $a = shift;
+	my $b = shift;
+
+	my $hamming = 0;
+	my $i;
+
+	return die("(?^:DNA strands must be of equal length)")
+		if ((length $a) != (length $b));
+
+	for $i (0..length($a)-1){
+    	if (substr($a, $i, 1) ne substr($b, $i, 1)){
+			$hamming++;
+		}
+    	   
+	}
+	return $hamming
 }
 1;
