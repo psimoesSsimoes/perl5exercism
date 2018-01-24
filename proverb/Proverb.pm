70b9230442f46d8db445104299c0267169d5382e
psimoes <pms@uninova.pt>
2018-01-24 15:03:46 +0000

    this is the solution i wanted to implement but just couldn't reach there

--- proverb/Proverb.pm
+++ proverb/Proverb.pm
@@ -3,27 +3,46 @@ use strict;
 use warnings;
 use Data::Dump 'dump';
 
+# sub proverb {
+#     my ($items, $qualifier) = @_;
+#     print STDERR "$items : $qualifier";
+#     my @proverb;
+
+#     $qualifier .= " " if $qualifier;
+#     print STDERR "\n";
+#     print STDERR "qualifier : $qualifier";
+
+#     print STDERR "\n";
+#     my $last_line = "And all for the want of a ${qualifier}$items->[0].";
+
+
+#     print STDERR "$last_line";
+#     print STDERR "\n";
+#     while ( @$items > 1 ) {
+#         push @proverb
+#              => sprintf("For want of a %s the %s was lost.", shift @$items, $items->[0]);
+#     }
+
+#     return join "\n" => (@proverb, $last_line)
+# }
+
 sub proverb {
-    my ($items, $qualifier) = @_;
-    print STDERR "$items : $qualifier";
-    my @proverb;
+	
+	my ($param, $qualifier) = @_;
 
-    $qualifier .= " " if $qualifier;
-    print STDERR "\n";
-    print STDERR "qualifier : $qualifier";
+	my $story = '';
+	
+	for(my $i = 0; $i < scalar(@$param) - 1; $i++){
+		$story .= sprintf("For want of a %s the %s was lost.\n", $$param[$i], $$param[$i + 1]);
+	}
 
-    print STDERR "\n";
-    my $last_line = "And all for the want of a ${qualifier}$items->[0].";
-
-
-    print STDERR "$last_line";
-    print STDERR "\n";
-    while ( @$items > 1 ) {
-        push @proverb
-             => sprintf("For want of a %s the %s was lost.", shift @$items, $items->[0]);
-    }
-
-    return join "\n" => (@proverb, $last_line)
+	$story .= sprintf("And all for the want of a %s%s.",  ($qualifier ? "$qualifier " : ''), $$param[0]);
+	
+	return $story;
 }
+
+
+1;
+
 
 1;
